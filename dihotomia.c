ea2f574e29f01ce98d8d274c32a08e616ba7d6d8
Yuliia Hetman <juliagetman5@gmail.com>
2019-11-12 22:22:04 +0200

    dihotomia

---
+++ dihotomia.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dihotomia.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhetman <yhetman@student.unit.ua>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 21:06:20 by yhetman           #+#    #+#             */
+/*   Updated: 2019/11/12 22:19:58 by yhetman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double	function(double x)
+{
+	double s;
+
+	s = sin(x);
+	return (x * x * x + s - 12 * x + 1);
+}
+
+int		sgn(double x)
+{
+	if (x < 0)
+		return (-1);
+	return ((x > 0) ? 1 : 0);
+}
+
+void	dihotomia(double eps, double a, double b)
+{
+	int		i;
+	double	last;
+	double	next;
+	double	res;
+
+	i = 0;
+	last = 0;
+	next = (a + b) / 2;
+	res = log2((a + b) / eps);
+	res = (int)(fabs(res) + 1);
+	while (abs(next - last) > eps)
+	{
+		last = next;
+		if (sgn(function(a)) == sgn(function(last)))
+			a = last;
+		if (sgn(function(b)) == sgn(function(last)))
+			b = last;
+		i++;
+		next = (a + b) / 2;
+	}
+	printf("|DIHOTOMIA METHOD|\nx* = %f\nPriori estimate: %d\nAsteriorI grade: %f\n",
+			next, i, res);
+}
+
+int main()
+{
+	double eps, a, b;
+
+	eps = 10 ^ (-3);
+	a = -0.9148;
+	b= 4.7587;
+
+	dihotomia(eps, a, b);
+}
